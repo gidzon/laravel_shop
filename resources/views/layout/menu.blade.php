05f09a44e482a4bcc629a61c2310e5cd342023d3
Nicolay Vorobev <sumakaki37rus@gmail.com>
2020-07-22 08:23:23 +0300

    поправил вывод категорий

--- resources/views/layout/menu.blade.php
+++ resources/views/layout/menu.blade.php
@@ -1,7 +1,15 @@
 @foreach($categories as $category)
-    @if($category->parent_id == 0)
-        <li><a href="{{route('product.index', $category->id)}}">{{$category->title}}</a></li>
-    @endif
 
+        <li><a href="{{route('product.index', $category->id)}}">{{$category->title}}</a></li>
+    @isset($category->children)
+        @foreach($category->children as $categoryChildren)
+            <ul>
+                <li><a href="{{route('product.index', $categoryChildren->id)}}">{{$categoryChildren->title}}</a></li>
+                @isset($categoryChildren->children)
+                    @include('layout.menu', ['categories' => $categoryChildren->children])
+                @endisset
+            </ul>
+        @endforeach
+    @endisset
 
 @endforeach
