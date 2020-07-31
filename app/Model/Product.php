5f9fa0afcf31abac51802c9015f78d79f1b10dbe
Nicolay Vorobev <sumakaki37rus@gmail.com>
2020-07-31 06:37:35 +0300

    добавил отношение

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -12,4 +12,9 @@ class Product extends Model
     {
         return $this->belongsTo(Category::class);
     }
+
+    public function feedbacks()
+    {
+        return $this->belongsTo('App\Feedback');
+    }
 }
